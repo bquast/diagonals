fff926d7114c320ce47cfade756fb44379d30cc3
Bastiaan Quast <bquast@gmail.com>
2015-05-22 11:33:45 +0200

    add minus block matrix tests

--- tests/testthat/test_minus_block_matrix.R
+++ tests/testthat/test_minus_block_matrix.R
@@ -1,12 +1,12 @@
-context("block matrix")
+context("minus block matrix")
 
 # create matrix
 m8x8 <- matrix(1:(8*8), nrow = 8)
 m9x9 <- matrix(1:(9*9), nrow = 9)
 
 # apply block_matrix()
-bm8x8 <- block_matrix(m8x8, 2)
-bm9x9 <- block_matrix(m9x9, 3)
+bm8x8 <- minus_block_matrix(m8x8, 2)
+bm9x9 <- minus_block_matrix(m9x9, 3)
 
 
 test_that("output size matches", {
@@ -15,6 +15,6 @@ test_that("output size matches", {
 })
 
 test_that("output content matches", {
-  expect_equal(rowSums(bm8x8), c(10, 12, 46, 48, 82, 84, 118, 120) )
-  expect_equal(rowSums(bm9x9), c(30, 33, 36, 120, 123, 126, 210, 213, 216) )
+  expect_equal(rowSums(bm8x8), c(222, 228, 202, 208, 182, 188, 162, 168) )
+  expect_equal(rowSums(bm9x9), c(303, 309, 315, 240, 246, 252, 177, 183, 189) )
 })
