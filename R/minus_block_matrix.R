689ccc73c6e74bfed9eda6e595a64ebf66613de5
Bastiaan Quast <bquast@gmail.com>
2015-05-22 12:27:53 +0200

    generalise minus_block_matrix and block_matrix from requiring square, to also accepting dimensions multiplied with STEP being equal to each other

--- R/minus_block_matrix.R
+++ R/minus_block_matrix.R
@@ -7,12 +7,41 @@
 
 minus_block_matrix <- function( x, step ) {
 
-  if (!dim(x)[1] == dim(x)[2]) stop("x is not a square matrix")
+  # check if square matrix
+  if ( dim(x)[1] == dim(x)[2]) {
 
-  for (j in 1:(dim(x)[1] %/% step) ) {
-    p <- seq( ((j-1)*step + 1), j*step )
-    x[p, p] <- 0
+    # remove square blocks
+    for (j in 1:(dim(x)[1] %/% step) ) {
+      p <- seq( ((j-1)*step + 1), j*step )
+      x[p, p] <- 0
+    }
+
+  # check if step multiplied with columns equals rows
+  } else if ( dim(x)[1] == (dim(x)[2]*step) ) {
+
+    # remove square blocks
+    for (j in 1:(dim(x)[1] %/% step) ) {
+      p <- seq( ((j-1)*step + 1), j*step )
+      q <- seq( ((j-1)*dim(x)[2]/(dim(x)[1]/step)+1), j*dim(x)[2]/(dim(x)[1]/step ) )
+      x[p, q] <- 0
+    }
+
+  # check if step multiplied with columns equals rows
+  } else if ( (dim(x)[1]*step) == dim(x)[2] ) {
+
+    # remove square blocks
+    for (j in 1:(dim(x)[2] %/% step) ) {
+      q <- seq( ((j-1)*step + 1), j*step )
+      p <- seq( ((j-1)*dim(x)[1]/(dim(x)[2]/step)+1), j*dim(x)[1]/(dim(x)[2]/step ) )
+      x[p, q] <- 0
+    }
+
+  # if nothing works, return error
+  } else {
+    stop("dimensions are not equal (matrix is not square), or integer multiples of step")
   }
 
+  # return output
   return(x)
+
 }
