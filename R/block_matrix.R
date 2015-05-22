d4172c94aabb8d446031fbd2147546a17c7298d5
Bastiaan Quast <bquast@gmail.com>
2015-05-22 11:16:01 +0200

    rename s to step

--- R/block_matrix.R
+++ R/block_matrix.R
@@ -1,14 +1,14 @@
 #' Block matrix
-#' 
+#'
 #' @name block_matrix
 #' @param x a square matrix
-#' @param s the dimensions (width = height) of the block matrix being selected over the diagonal of matrix x
+#' @param step the dimensions (width = height) of the block matrix being selected over the diagonal of matrix x
 #' @export
 
-block_matrix <- function( x, s ) {
-  
-  y <- minus_block_matrix(x, s)
-  
+block_matrix <- function( x, step ) {
+
+  y <- minus_block_matrix(x, step)
+
   return(x - y)
-  
+
 }
--- R/minus_block_matrix.R
+++ R/minus_block_matrix.R
@@ -1,18 +1,18 @@
 #' Minus Block matrix
-#' 
+#'
 #' @name minus_block_matrix
 #' @param x a square matrix
-#' @param s the dimensions (width = height) of the block matrix being dropped over the diagonal of matrix x
+#' @param step the dimensions (width = height) of the block matrix being dropped over the diagonal of matrix x
 #' @export
 
-minus_block_matrix <- function( x, s ) {
-  
+minus_block_matrix <- function( x, step ) {
+
   if (!dim(x)[1] == dim(x)[2]) stop("x is not a square matrix")
-  
-  for (j in 1:(dim(x)[1] %/% s) ) {
-    p <- seq( ((j-1)*s + 1), j*s )
+
+  for (j in 1:(dim(x)[1] %/% step) ) {
+    p <- seq( ((j-1)*step + 1), j*step )
     x[p, p] <- 0
   }
-  
+
   return(x)
 }
