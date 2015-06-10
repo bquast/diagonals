10477dac340ac4b65802d5ce0d9920fe129ba39d
Bastiaan Quast <bquast@gmail.com>
2015-06-10 10:54:37 +0200

    document `diag<-`

--- R/diag.R
+++ R/diag.R
@@ -1,3 +1,13 @@
+#' Matrix Diagonals
+#'
+#' @description A small extension of the `diag<-` function from the base package, allowing for replacement vectors smaller than the diagonal.
+#' @param x a matrix, vector or 1D array, or missing.
+#' @param value either a single value or a vector of length equal to that of the current diagonal. Should be of a mode which can be coerced to that of x.
+#' @details The `diag<-` function in the base package allows for two type of value, either of length 1, or of length equal to the length of the diagonal
+#' This function extends that by allowing
+#' @export
+
+
 `diag<-` <- function (x, value) {
   dx <- dim(x)
   if (length(dx) != 2L)
