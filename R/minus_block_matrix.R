42c812ecdcfec91bbdc9c572e3da6bf1008366e9
Bastiaan Quast <bquast@gmail.com>
2015-06-11 15:00:36 +0200

    update to work with off diagonal

--- R/minus_block_matrix.R
+++ R/minus_block_matrix.R
@@ -5,6 +5,7 @@
 #' @param steps the required number of steps (block matrices) across the diagonal
 #' @param size the width or height of the matrix being dropped over the diagonal of matrix x
 #' @param replacement value to be inserted on the diagonal, by default this is zero (0).
+#' @param off_diagonal should the operation be apply to the elements off the fat diagonal.
 #' @param byrow should replacement occur by row, alternatively this is done by column (only relevant for replacement vectors).
 #' @details Either steps or size is expected to be provided.
 #' @export
@@ -19,7 +20,7 @@
 #' minus_block_matrix(m, steps=4)
 
 
-minus_block_matrix <- function( x, steps = NULL, size = NULL, replacement = 0, byrow = TRUE ) {
+minus_block_matrix <- function( x, steps = NULL, size = NULL, replacement = 0, off_diagonal=FALSE, byrow = TRUE ) {
 
   # save dimensions
   dx <- dim(x)
@@ -33,12 +34,6 @@ minus_block_matrix <- function( x, steps = NULL, size = NULL, replacement = 0, b
   # check if square matrix
   if ( dx[1] != dx[2])
     stop("not a square matrix")
-
-  # check that dimensions of this square matrix are a multiple of size
-  if(dx[1] %% size != 0) warning("Matrix dimensions are not a multiple of size, problems will occur in the bottom right (South-East) of the output matrix.")
-
-  if (as.integer(size^2*steps) %% lr != 0 && lr != 1L)
-    stop("replacment fat diagonals has wrong length")
 
   # determine the size of the step
   if ( !is.null(size) ) {
@@ -69,9 +64,20 @@ minus_block_matrix <- function( x, steps = NULL, size = NULL, replacement = 0, b
 
     # set to unit
     size <- 1L
+
+
+    # create steps
+    steps <- dx[1] %/% size
     }
 
   }
+
+  # check that dimensions of this square matrix are a multiple of size
+  if(dx[1] %% size != 0) warning("Matrix dimensions are not a multiple of size, problems will occur in the bottom right (South-East) of the output matrix.")
+
+  if (as.integer(size^2*steps) %% lr != 0 && lr != 1L)
+    stop("replacment fat diagonals has wrong length")
+
 
   # split dimension according to steps
   spl <- split_vector(1:dx[1], steps = steps)
@@ -80,11 +86,19 @@ minus_block_matrix <- function( x, steps = NULL, size = NULL, replacement = 0, b
   a <- vector()
   b <- vector()
 
-  # insert combinations
-  for (i in 1:steps) {
-    a <- c(a, rep(spl[[i]], times=size) )
-    b <- c(b, rep(spl[[i]], each=size) )
-  }
+  if (!off_diagonal) {
+    for (i in 1:steps) {
+      a <- c(a, rep(spl[[i]], times=dx[1]-size) )
+      b <- c(b, rep(setdiff(1:dx[1], spl[[i]]), each=size) )
+      }
+    } else {
+    # insert combinations
+    for (i in 1:steps) {
+      a <- c(a, rep(spl[[i]], times=size) )
+      b <- c(b, rep(spl[[i]], each=size) )
+      }
+    }
+
 
   # transpose matrix if byrow is false (transpose back after replacement)
   if(!byrow) x <- t(x)
