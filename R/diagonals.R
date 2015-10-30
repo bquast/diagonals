dc5c801ec0561eaee0b88f58369da807bed521be
Bastiaan Quast <bquast@gmail.com>
2015-10-30 08:45:40 +0100

    add description

--- R/diagonals.R
+++ R/diagonals.R
@@ -2,6 +2,13 @@
 #' @docType package
 #' @title diagonals
 #' @author Bastiaan Quast \email{bquast@@gmail.com}
+#' @description Several tools for handling block-matrix diagonals and similar
+#' constructs are implemented. Block-diagonal matrices can be extracted or removed
+#' using two small functions implemented here. In addition, non-square matrices
+#' are supported. Block diagonal matrices occur when two dimensions of a data set
+#' are combined along one edge of a matrix. For example, trade-flow data in the
+#' decompr' and 'gvc' packages have each country-industry combination occur along
+#' both edges of the matrix.
 #' @seealso http://qua.st/diagonals
 NULL
 .onAttach <- function(...) {
